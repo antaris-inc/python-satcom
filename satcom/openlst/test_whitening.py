31c30dced2eda1c2bfe9c304cbaad1e563e95331
Louise Tamondong <louisetamondong@Louises-MacBook-Pro.local>
2024-10-01 13:00:14 -0700

    add dewhitening test

--- satcom/openlst/test_whitening.py
+++ satcom/openlst/test_whitening.py
@@ -12,3 +12,13 @@ class TestWhitening(unittest.TestCase):
         want = b'\x99\x8er\xf8\x8c\xf7'
 
         self.assertEqual(got, want, f'unexpected result: want={want} got={got}')
+
+    def test_dewhitening(self):
+        """Validates data de-whitening"""
+        bs = b'\x90\x91x\xf4\x81\xf6G'
+        gen = whitening.pn9()
+
+        got = whitening.whiten(bs, gen)
+        want = bytes(b'openlst')
+
+        self.assertEqual(got, want, f'unexpected result: want={want} got={got}')
